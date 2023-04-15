994d0755a76833663cfe4cff3e694521db82e11f
Gunel <kocharlig@gmail.com>
2023-04-16 00:05:45 +0400

    Home Work

---
+++ T1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Add digit number- weekday ");
+int daynumber = int.Parse(Console.ReadLine());
+
+  if (daynumber == 6 || daynumber == 7) {
+  Console.WriteLine("(Weekend) -> Yeap!");
+  }
+   else {
+Console.WriteLine("(Weekend) -> No!");
+}
---
+++ T2/Program.cs
@@ -0,0 +1,4 @@
+﻿System.Console.WriteLine("Add threedigit number");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Secondnumber -> "+stringNumber[1]);
